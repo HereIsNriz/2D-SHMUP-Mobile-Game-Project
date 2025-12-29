594351f5a07e231afa4369048594c52401f2d308
Nriz <here.is.nriz@gmail.com>
2025-12-29 21:13:53 +0700

    Moving background
    - Make the ground and the floor moving to down and reset their position if reaching limit
    - Change the ground and the floor sprite to 16x16 pixels

---
+++ 2D-SHMUP-Mobile-Game-Project/Assets/Scripts/MovingEnvironment.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnvironment : MonoBehaviour
+{
+    //
+
+    //
+    [SerializeField] private float speed;
+    //
+    private Rigidbody2D m_envinronmentRb;
+    private GameManager m_gameManager;
+    private Vector2 m_startPosition;
+    private float m_resetPosition = -2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_envinronmentRb = GetComponent<Rigidbody2D>();
+        m_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        m_startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y <= m_resetPosition)
+        {
+            transform.position = m_startPosition;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_gameManager.IsGameRunning)
+        {
+            MoveGroundAndFloor();
+        }
+    }
+
+    private void MoveGroundAndFloor()
+    {
+        m_envinronmentRb.velocity = Vector2.down * speed * Time.deltaTime;
+    }
+}
