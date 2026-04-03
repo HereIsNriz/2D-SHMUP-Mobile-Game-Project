6fdae1c5b5d58e9b62c43505e21cf27acdb02c8d
Nriz <here.is.nriz@gmail.com>
2026-04-03 20:15:40 +0700

    Exit App Statement
    In Menu Scene add an exit app statement, if the player press the back button on their phone there will be a massage to ensuring the player if they are really want to exit, below the massage the are two buttons the Yes button and the No button, if the player press the No button the massage will disappear and the player can continue playing, and if the player press the Yes button then the application will exit

--- 2D-SHMUP-Mobile-Game-Project/Assets/Scripts/MenuManager.cs
+++ 2D-SHMUP-Mobile-Game-Project/Assets/Scripts/MenuManager.cs
@@ -6,20 +6,57 @@ using UnityEditor;
 
 public class MenuManager : MonoBehaviour
 {
+    //
+
+    //
+    [SerializeField] private GameObject m_exitAppPanel;
+
+    //
+    private bool m_isExitAppPanelShowing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_isExitAppPanelShowing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ShowExitAppPanel();
     }
 
     public void PlayButton()
     {
         SceneManager.LoadScene(2);
+    }
+
+    private void ShowExitAppPanel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !m_isExitAppPanelShowing)
+        {
+            m_exitAppPanel.gameObject.SetActive(true);
+            m_isExitAppPanelShowing = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && m_isExitAppPanelShowing)
+        {
+            m_exitAppPanel.gameObject.SetActive(false);
+            m_isExitAppPanelShowing = false;
+        }
+    }
+
+    public void YesButton()
+    {
+        //Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+
+    public void NoButton()
+    {
+        if (m_isExitAppPanelShowing)
+        {
+            m_exitAppPanel.gameObject.SetActive(false);
+            m_isExitAppPanelShowing = false;
+        }
     }
 }
