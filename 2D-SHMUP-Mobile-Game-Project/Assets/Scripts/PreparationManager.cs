f6f06affc7d100b8b074add3412f72c2d44d10ec
Nriz <here.is.nriz@gmail.com>
2026-04-22 09:31:04 +0700

    Preparation Scene SFX
    In preparation scene add a SFX for Back and Next button.

--- 2D-SHMUP-Mobile-Game-Project/Assets/Scripts/PreparationManager.cs
+++ 2D-SHMUP-Mobile-Game-Project/Assets/Scripts/PreparationManager.cs
@@ -11,10 +11,14 @@ public class PreparationManager : MonoBehaviour
     //
     [SerializeField] private TextMeshProUGUI m_coinsText;
     [SerializeField] private TextMeshProUGUI m_highScoreText;
+    [SerializeField] private AudioSource m_backButtonSound;
+    [SerializeField] private AudioSource m_nextButtonSound;
     [SerializeField] private int m_highScore;
     [SerializeField] private int m_totalCoinsEarned;
 
     //
+    private float m_backButtonSoundDuration = 0.2f;
+    private float m_nextButtonSoundDuration = 0.4f;
 
     // Start is called before the first frame update
     void Start()
@@ -43,11 +47,25 @@ public class PreparationManager : MonoBehaviour
 
     public void BackButton()
     {
+        m_backButtonSound.PlayOneShot(m_backButtonSound.clip, 1f);
+        StartCoroutine(BackButtonSound());
+    }
+
+    private IEnumerator BackButtonSound()
+    {
+        yield return new WaitForSeconds(m_backButtonSoundDuration);
         SceneManager.LoadScene(0);
     }
 
     public void NextButton()
     {
+        m_nextButtonSound.PlayOneShot(m_nextButtonSound.clip, 1f);
+        StartCoroutine(NextButtonSound());
+    }
+
+    private IEnumerator NextButtonSound()
+    {
+        yield return new WaitForSeconds(m_nextButtonSoundDuration);
         SceneManager.LoadScene(1);
     }
 }
